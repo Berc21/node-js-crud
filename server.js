ad80b5281f8089f3bf8c88438f3ba34562a07eb0
Berkan Çetinkaya <berkan.cetinkaya@windowslive.com>
2018-03-31 00:48:30 +0300

    init

---
+++ server.js
@@ -0,0 +1,44 @@
+
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv').config();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+var db
+
+
+
+MongoClient.connect(process.env.DB_HOST_URL, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db(process.env.DB_NAME) // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+});
+
+
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      // renders index.ejs
+      res.render('index.ejs', {quotes: result})
+    })
+  })
+  
+  
+  app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
+
+
+ 
